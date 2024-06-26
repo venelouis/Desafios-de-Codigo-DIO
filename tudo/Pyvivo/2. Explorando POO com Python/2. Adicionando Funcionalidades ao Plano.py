2124ac186fe1606bfb74bdca2afe4de161ad7a40
venelouis <venelouistyago@gmail.com>
2024-06-26 16:06:35 -0300

    feitos

--- tudo/Pyvivo/2. Explorando POO com Python/2. Adicionando Funcionalidades ao Plano.py
+++ tudo/Pyvivo/2. Explorando POO com Python/2. Adicionando Funcionalidades ao Plano.py
@@ -1,3 +1,79 @@
+'''Desafio
+Agora, vamos Adicionar uma funcionalidade à classe UsuarioTelefone para que possa ser verificado o saldo disponível em seu plano. Para essa solução, você pode criar uma classe PlanoTelefone, o seu método de inicialização e encapsular os atributos, 'nome' e 'saldo' dentro da classe. Adicione também um método 'verificar_saldo' para verificar o saldo do plano e uma  'mensagem_personalizada' para gerar uma mensagem personalizada.
+
+Condições da verificação do saldo:
+- Caso o saldo seja menor do que 10, retorne: "Seu saldo está baixo. Recarregue e use os serviços do seu plano."
+- Caso o saldo seja maior ou igual a 50, retorne: "Parabéns! Continue aproveitando seu plano sem preocupações."
+- Caso contrário, retorne: "Seu saldo está razoável. Aproveite o uso moderado do seu plano."
+
+Entrada:
+Como entrada, será solicitado o nome, plano (Essencial, Prata, Premium) e saldo atual do cliente.
+
+Saída:
+Mensagem personalizada de acordo o saldo do cliente.
+
+Exemplos;
+A tabela abaixo apresenta exemplos com alguns dados de entrada e suas respectivas saídas esperadas. Certifique-se de testar seu programa com esses exemplos e com outros casos possíveis.
+
+Entrada:
+João     
+Essencial      
+9
+
+Saída:
+Seu saldo está baixo. Recarregue e use os serviços do seu plano.
+
+Entrada:
+Debora
+Prata
+11
+
+Saída:
+Seu saldo está razoável. Aproveite o uso moderado do seu plano.
+
+Entrada:
+Catarina
+Premium
+50
+
+Saída:
+Parabéns! Continue aproveitando seu plano sem preocupações.
+'''
+#Código fornecido pelo desafio:
+'''
+# TODO: Crie a classe PlanoTelefone, seu método de inicialização e encapsule os atributos, 'nome' e 'saldo':
+
+
+# TODO: Crie um método 'verificar_saldo' para verificar o saldo do plano sem acessar diretamente o atributo:    
+    
+# TODO: Crie um método 'mensagem_personalizada' para gerar uma mensagem personalizada com base no saldo: 
+    
+
+# Classe UsuarioTelefone:
+class UsuarioTelefone:
+    def __init__(self, nome, plano):
+        self.nome = nome
+        self.plano = plano
+
+# TODO: Crie um método para verificar o saldo do usuário e gerar uma mensagem personalizada:
+ 
+
+
+# Recebendo as entradas do usuário (nome, plano, saldo):
+nome_usuario = input()
+nome_plano = input()
+saldo_inicial = float(input())
+
+ # Criação de objetos do plano de telefone e usuário de telefone com dados fornecidos:
+plano_usuario = PlanoTelefone(nome_plano, saldo_inicial) 
+usuario = UsuarioTelefone(nome_usuario, plano_usuario)  
+
+# Chamada do método para verificar_saldo sem acessar diretamente os atributos do plano:
+saldo_usuario, mensagem_usuario = usuario.verificar_saldo()  
+print(mensagem_usuario)
+'''
+
+
 # Classe PlanoTelefone
 class PlanoTelefone:
     def __init__(self, nome, saldo):
--- tudo/Pyvivo/2. Explorando POO com Python/3. Realizando Chamadas.py
+++ tudo/Pyvivo/2. Explorando POO com Python/3. Realizando Chamadas.py
@@ -1,9 +1,106 @@
+'''Desafio
+Vamos agora, adicionar uma funcionalidade à classe UsuarioTelefone, que realizar chamadas para outros usuários. Cada chamada terá uma duração em minutos e o custo será deduzido do saldo do usuário, suponha o custo de $0.10 por minuto. Você pode criar um método fazer_chamada que vai permitir que o usuário faça a chamada, ele vai receber o destinatario e duracao como parâmetros. Calcule o custo da chamada usando o método 'custo_chamada' do objeto 'plano', além de adicionar o método deduzir_saldo para deduzir o valor do saldo do plano e depois retorne uma mensagem adequada como mostra no exemplo a baixo.
+
+Entrada
+Número do usuário, número do telefone, saldo inicial, número do destinatário e a duração da chamada em minutos.
+
+Saída
+Mensagem indicando o sucesso da chamada ou saldo insuficiente para fazer a chamada.
+
+Exemplos
+A tabela abaixo apresenta exemplos com alguns dados de entrada e suas respectivas saídas esperadas. Certifique-se de testar seu programa com esses exemplos e com outros casos possíveis.
+
+Entrada:
+Rodrigo
+(00) 90000-0000
+10.00
+(33) 93333-3333
+60
+
+Saída:
+Chamada para (33) 93333-3333 realizada com sucesso. Saldo: $4.00
+
+Entrada:
+Yule
+(11) 91111-1111
+30.00
+(00) 90000-0000
+240
+
+Saída:
+Chamada para (00) 90000-0000 realizada com sucesso. Saldo: $6.00
+
+Entrada:
+Amelia
+(33) 93333-3333
+10.00
+(11) 91111-1111
+120
+
+Saída:
+Saldo insuficiente para fazer a chamada.
+'''
+# Código fornecido pelo desafio:
+'''
+# Classe UsuarioTelefone e o encapsulamento dos atributos nome, numero e plano:
 class UsuarioTelefone:
     def __init__(self, nome, numero, plano):
         self.nome = nome
         self.numero = numero
         self.plano = plano
 
+# TODO: Crie um método fazer_chamada para permitir que um usuário faça uma chamada telefônica:
+
+# TODO: Calcule o custo da chamada usando o método 'custo_chamada' do objeto 'plano':
+
+# TODO: Verifique se o saldo do plano é suficiente para a chamada.
+
+# TODO: Se o saldo for suficiente, deduz o custo da chamada do saldo do plano.
+
+# TODO: E retorne uma mensagem de sucesso com o destinatário e o saldo restante após a chamada:
+
+
+# Classe Pano, ela representa o plano de um usuário de telefone:
+class Plano:
+    def __init__(self, saldo_inicial):
+        self.saldo = saldo_inicial
+
+# TODO: Crie um método para verificar_saldo e retorne o saldo atual:
+
+# TODO: Crie um método custo_chamada para calcular o custo de uma chamada supondo o custo de $0.10 por minuto:
+
+
+# TODO: Crie um método deduzir_saldo para deduz o valor do saldo do plano:
+
+
+# Classe UsuarioPrePago, aqui vemos a herança onde UsuarioPrePago herda os atributos e métodos da classe UsuarioTelefone:
+class UsuarioPrePago(UsuarioTelefone):
+    def __init__(self, nome, numero, saldo_inicial):
+        super().__init__(nome, numero, Plano(saldo_inicial))
+
+
+# Recebendo as informações do usuário:
+nome = input()
+numero = input()
+saldo_inicial = float(input())
+
+# Objeto de UsuarioPrePago com os dados fornecidos:
+usuario_pre_pago = UsuarioPrePago(nome, numero, saldo_inicial)
+destinatario = input()
+duracao = int(input())
+
+# Chama o método fazer_chamada do objeto usuario_pre_pago e imprime o resultado:
+print(usuario_pre_pago.fazer_chamada(destinatario, duracao))
+
+'''
+
+
+
+class UsuarioTelefone:
+    def __init__(self, nome, numero, plano):
+        self.nome = nome
+        self.numero = numero
+        self.plano = plano
 
     def fazer_chamada(self, destinatario, duracao):
         custo_chamada = self.plano.custo_chamada(duracao)
@@ -13,34 +110,22 @@ class UsuarioTelefone:
         else:
             return "Saldo insuficiente para fazer a chamada."
 
-
-
-
 class Plano:
     def __init__(self, saldo_inicial):
         self.saldo = saldo_inicial
 
-
     def verificar_saldo(self):
         return self.saldo
-
 
     def custo_chamada(self, duracao):
         return duracao * 0.10
 
-
     def deduzir_saldo(self, valor):
         self.saldo -= valor
-
-
-
 
 class UsuarioPrePago(UsuarioTelefone):
     def __init__(self, nome, numero, saldo_inicial):
         super().__init__(nome, numero, Plano(saldo_inicial))
-
-
-
 
 nome = input()
 numero = input()
